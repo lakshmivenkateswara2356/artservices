40604a45ccc11e42205a474ace6eefaa757a978d
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-05-20 02:17:23 +0530

    commitsett

--- src/components/ServiceDescription/index.js
+++ src/components/ServiceDescription/index.js
@@ -26,24 +26,20 @@ const ServiceDescription = () => (
 The cargo area of the truck should be clean and free of any loose objects/particles.
 Works should be crated for transportation, as per Museum specifications.
 Crates should be strapped or wedged so that they cannot move about when in transit.
-There should be a minimum of two UAPL staff with the works in transit, at all times, other than when the shipments are on board vessel or on the aircraft or accompanied by a courier designated by the lender/Museum/Art Gallery/Organiser.
-Loading/Unloading of works into the trucks in strong wind or rain is to be avoided, unless required in an emergency.      
+There should be a minimum of two UAPL staff with the works iunless required in an emergency.      
         </ul>
       </div>
       <div className="sectionsec">
         <h2 className="transco">Airport Supervision</h2>
         <ul className="noreste">
           <li>To handle successful fine art consignment...</li>
-          To handle successful fine art consignment is to have maximum control on the consignment starting from packing, handling, transportation and handover to airlines.
-The key is to go one step more and oversee the handling at airport, 
+          To handle successful fine art  
           <li>Permissions for overseeing the handling...</li>
-          witnessing the palletizing / de-palletizing of the consignment.
-Permissions for overseeing the handling at airport can be obtained at the time of filing the documents at Customs (depending on the security situation at each city).
+          witnessing the palletizing / the time of filing the documents at Customs (depending on the security situation at each city).
 Professional Art handlers for examination & x-ray handling of crates.
           <li>Professional Art handlers for examination...</li>
           witnessing the palletizing / de-palletizing of the consignment.
-Permissions for overseeing the handling at airport can be obtained at the time of filing the documents at Customs (depending on the security situation at each city).
-Professional Art handlers for examination & x-ray handling of crates.
+Permissions for overseeing the handling at airport c
         </ul>
       </div>
     </div>
