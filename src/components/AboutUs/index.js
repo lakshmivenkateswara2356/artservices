4add0c329685a0f8307a74a1fa412d3a4c2b2dde
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-05-20 01:55:19 +0530

    commitaboutus

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -5,7 +5,7 @@ const AboutUs =() => (
     <div>
         <div className="containerabout">
         <h1 className="mainheddabout">Who we are?</h1>
-        <p>  India’s leading, oldest & largest specialist Fine Art Handling Company
+        <p className="firstarranfee">  India’s leading, oldest & largest specialist Fine Art Handling Company
 A decade of experience in handling Fine Arts.
 Multi-location domestic network, covering the entire Indian sub-continent. Offices in Delhi, Mumbai, Bangalore, Chennai, Hyderabad and Kolkata.
 Official fine art handling agent for India Art Fair (formerly known India Art Summit) 2009 to 2011, India Art Collective 2011, India Art Festival 2011.</p>
