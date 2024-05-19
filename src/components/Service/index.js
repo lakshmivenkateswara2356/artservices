5c6bb021bd1fa6f5294e6cf1d3edd109e940aaea
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-05-20 01:52:00 +0530

    addedcorrecrcommit

--- src/components/Service/index.js
+++ src/components/Service/index.js
@@ -14,8 +14,9 @@ const Service =() => (
 <li className="listitemdj1">The cargo area of the truck should be clean and free of any loose objects/particles.</li>
 <li className="listitemdj2">Works should be crated for transportation, as per Museum specifications.</li>
 </div>
-<div className="firstarranf">
 <p className="tramdsdk">Airport Supervision:</p>
+<div className="firstarranf">
+
 <li className="listitemdj">To handle successful fine art consignment is to have maximum control on the consignment starting from packing, handling, transportation and handover to airlines.ne and an internal strapping system.  A supply of clean PU Foam, bubble wrap and acid free tissue should be carried.</li>
 <li className="listitemdj3">The key is to go one step more and oversee the handling at airport, witnessing the palletizing / de-palletizing of the consignment.</li>
 <li className="listitemdj2">Professional Art handlers for examination & x-ray handling of crates.</li>
